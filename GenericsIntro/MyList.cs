10cc7e7e6d2fffce94806e7e61ebf0c886843f59
berkayakkocc <berkayakkocc6@gmail.com>
2022-06-29 09:04:33 +0300

    Part2 generics

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -10,24 +10,24 @@ namespace GenericsIntro
     {
         T[] items;
         //Constructor : Bir kere çalışır
-        public MyList()
+        public MyList()//Constructor
         {
             items = new T[0];
         }
         public void Add(T item) 
         {
-            T[] tempArray = items;
+            T[] tempArray = items;//geçici bir diziye atıyor
             items = new T[items.Length + 1];
             for (int i = 0; i < tempArray.Length; i++)
             {
-                items[i] = tempArray[i];
+                items[i] = tempArray[i];//geçici dizinin uzunlugu kadar itemsı alıp eşitliyor
             }
            
             items[items.Length - 1] = item;
         }
 
         public int Count 
-        { get { return items.Length; }  }
+        { get { return items.Length; }  }//itemsın uzunlugunu gönderiyor
 
         public T[] Items { get { return items; }  }
     }
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -9,10 +9,19 @@ namespace GenericsIntro
         {
             MyList<string> names = new MyList<string>();
 
-            names.Add("Berkay");
+            names.Add("Berkay");//Add methodunu çağırıyor
+            Console.WriteLine(names.Count);//değişkenin içini saydırıyo
 
-          
+            names.Add("Ugur");
+            Console.WriteLine(names.Count);
 
+            names.Add("Serhat");
+            Console.WriteLine(names.Count);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);//tum elemanı yazdırıyor
+            }
         }
     }
 }
